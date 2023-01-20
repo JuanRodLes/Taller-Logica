503d96cc32ef26debdf314f7449ae74c0cbe53d3
JuanRodLes <juan1503esteban@gmail.com>
2023-01-20 16:24:03 -0500

    Finish

--- Assets/Js/scripts.js
+++ Assets/Js/scripts.js
@@ -10,27 +10,65 @@ function sumatoria() {
 }
 
 function parimpar() {
-    let numero_ingresado = document.querySelector("#numero").value
-    let revisar = numero_ingresado / 2
-    if (revisar == 0) {
-        document.querySelector("#verificar").innerHTML = "Impar"
+    let num_ingresado = document.getElementById("NumeroIngresado").value
+    if (num_ingresado % 2 == 0) {
+        document.getElementById("verificar").innerHTML = "Par"
     } else {
-        document.querySelector("#verificar").innerHTML = "Par"
+        document.getElementById("verificar").innerHTML = "Impar"
     }
 }
 
 function pizzeria() {
-    let edad = document.querySelector("#edadIngresada").value
-    let genero = document.querySelector("input.radio")
-    if (edad <= 10) {
-        document.querySelector("#reclame").innerHTML = "Reclame un jugo"
-    }
-    else (edad >= 18) {
-        document.querySelector("#reclame").innerHTML = "Reclame una cerveza"
-    }
-    if (genero = getElementById("male")) {
-        document.getElementById("reclame").innerHTML += "y una porción de pizza tres carnes"
+    let edad = document.getElementById("edadIngresada").value
+    let genero = document.getElementById("generos").value
+
+    if (edad <= 0) {
+        alert("Señor usuario, usted no ha nacido")
     } else {
-        document.getElementById("reclame").innerHTML += "y una porción de pizza hawaiana"
+        if (edad <= 10) {
+            document.getElementById("bebida").innerHTML = "🧃 Reclame un jugo 🧃"
+        } else if (edad <= 17) {
+            document.getElementById("bebida").innerHTML = "☹ Desafortunadamente no recibe ninguna bebida ☹"
+        } else {
+            document.getElementById("bebida").innerHTML = "🍺 Reclame una cerveza 🍺"
+        }
+
+    }
+
+    if (genero != "none") {
+        if (genero == "Male") {
+            document.getElementById("porcion").innerHTML = "🍕 Reclame una porción de pizza tres carnes 🍕"
+        } else {
+            document.getElementById("porcion").innerHTML = "🍕 Reclame una porción de pizza Hawaiana 🍕"
+        }
+    } else {
+        alert("Por favor indique su género")
+    }
+}
+
+function multiplicaciones() {
+    let multiplicado = document.getElementById("numero1").value
+    let multiplicador = document.getElementById("numero2").value
+    let resultadosMultiplicacion = ""
+    for (let x = 1; x <= multiplicador; x++) {
+        resultadosMultiplicacion = resultadosMultiplicacion + (multiplicado + "x" + x + "=" + (multiplicado * x) + "<br>")
+        document.getElementById("resultadox").innerHTML = resultadosMultiplicacion
+    }
+}
+
+function Descuento() {
+    let PromedioIngresado = document.getElementById("PromedioEstudiante").value
+    let Total = 1000000
+    let Promedio3a4 = (Total * 5) / 100
+    let Promedio5 = (Total * 50) / 100
+
+    if (PromedioIngresado <= 2) {
+        document.getElementById("DescuentoMatricula").innerHTML = `$${Total}`
+    } else {
+        if (PromedioIngresado < 5) {
+            document.getElementById("DescuentoMatricula").innerHTML = `$${Total - Promedio3a4}`
+        } else {
+            document.getElementById("DescuentoMatricula").innerHTML = `$${Total - Promedio5}`
+        }
     }
 }
